d4fcca1a7fbad9c9c01a4fd46d339c51ca75fe11
crazycakiem <crazycakiem@gmail.com>
2017-11-24 18:08:10 +0700

    update 241117
    Update Premium Club

--- PremiumClub/js/all.js
+++ PremiumClub/js/all.js
@@ -279,7 +279,14 @@ Sliders
 function initPageSliders(){
  (function($){
   "use strict";
-
+  // Fullwidth slider
+  $(".fullwidth-slider").owlCarousel({
+      slideSpeed: 350,
+      singleItem: true,
+      autoHeight: true,
+      navigation: true,
+      navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
+  });
   // Item carousel
   $(".item-carousel").owlCarousel({
    autoPlay: true,
